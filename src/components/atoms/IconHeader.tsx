cc1ba9a5fa62cdefe297c808a6407acfa6ca714e
hideki-okawa <h.okawa0303@gmail.com>
2022-10-25 09:08:22 +0900

    Iconのエラーを修正

--- src/components/atoms/IconHeader.tsx
+++ src/components/atoms/IconHeader.tsx
@@ -2,13 +2,28 @@ import { Header, Icon } from "semantic-ui-react";
 
 type Props = {
 	title: string;
+	icon: string;
 };
 
+type IconProps = {
+	name: string;
+};
+
+function HeaderIcon(props: IconProps) {
+	switch (props.name) {
+		case "question":
+			return <Icon name="question" />;
+		case "answer":
+			return <Icon name="font" />;
+		default:
+			return <Icon name="font" />;
+	}
+}
 export default function IconHeader(props: Props) {
 	return (
 		<>
 			<Header as="h2" dividing>
-				<Icon name="question" />
+				<HeaderIcon name="question" />
 				<Header.Content>{props.title}</Header.Content>
 			</Header>
 		</>
